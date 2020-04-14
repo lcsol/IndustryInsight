c6d7977d155043c6932f2aa8ff5f139c4e0d94f1
Chao Lin <chaolin@Chaos-MacBook-Pro.local>
2020-04-14 10:55:14 -0400

    clean code

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -1,119 +1,3 @@
-// import React from 'react'
-// import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput, TouchableOpacityBase, Button } from 'react-native'
-// import fbKey from '../API/Fire'
-// // import { addPost } from '../API/PostServiceAPI'
-// import * as firebase from 'firebase'
-// import 'firebase/firestore';
-// import config from '../config'
-
-// if (!firebase.app.length) {
-//     firebase.initializeApp(config.firebaseConfig)
-// }
-
-// // const docRef = firebase.firestore().collection('posts')
-// export default class PostScreen extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = ({
-//             text: '',
-//             loading: false
-//         });
-//         // this.docRef = firebase.firestore.collection('posts');
-//         this.userId = firebase.auth().currentUser.uid;
-//     }
-//     // addPost = async ({ text }) => {
-//     //         return new Promise((res, rej) => {
-//     //             this.firestore
-//     //                 .collection("posts")
-//     //                 .add({
-//     //                     text,
-//     //                     uid: this.uid,
-//     //                     timestamp: this.timestamp
-//     //                 })
-//     //                 .then(ref => {
-//     //                     res(ref)
-//     //                 })
-//     //                 .catch(error => {
-//     //                     rej(error)
-//     //                 });
-//     //         });
-//     //     };
-
-
-//     handlePost = () => {
-//         const db = firebase.firestore();
-//         return new Promise((res, rej) => {
-//             console.log("before");
-//             // firebase.firestore().collection('posts').add({
-//             //     request: this.state.text,
-//             //     timestamp: firebase.firestore.FieldValue.serverTimestamp()
-
-//             // }).then(() => {console.log("success");})
-//             // .catch((error) => { 
-//             //     console.log(error);
-//             //     console.log("failed");
-//             // })
-//             // ;
-
-//             db.collection("posts").doc("").set({
-//                 request: this.state.text,
-//                 timestamp: firebase.firestore.FieldValue.serverTimestamp()
-
-//             })
-//             console.log("after");
-//         })
-//     }
-
-
-
-
-
-//     render() {
-//         return (
-//             <SafeAreaView>
-//                 <View style={styles.container}>
-//                     <View style={styles.inputContainer}>
-//                         <TextInput
-//                             autoFocus={true}
-//                             multiline={true}
-//                             numberOfLines={4}
-//                             placeholder="What's on your mind?"
-//                             style={{ flex: 1, backgroundColor: 'white' }}
-//                             onChangeText={text => this.setState({ text })}
-//                             value={this.state.text}
-//                         />
-//                     </View>
-//                     <View style={styles.footer}>
-//                         <TouchableOpacity onPress={this.handlePost.bind(this)}>
-//                             <Text style={{ height: 100, fontWeight: "500", color: 'blue' }}>Post</Text>
-//                         </TouchableOpacity>
-//                     </View>
-//                 </View>
-//             </SafeAreaView>
-//         )
-//     }
-// }
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         alignItems: 'center'
-//     },
-//     inputContainer: {
-//         margin: 5,
-//         flexDirection: 'row',
-//         height: 300
-//     },
-//     footer: {
-//         flexDirection: 'row',
-//         justifyContent: "space-between",
-//         paddingHorizontal: 32,
-//         paddingVertical: 12
-//     }
-// })
-
-
-
-
 import React from 'react';
 import {
     View,
@@ -122,7 +6,8 @@ import {
     TouchableOpacity,
     SafeAreaView,
     TextInput,
-    Image
+    Image,
+    Button
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Fire from '../API/Fire';
@@ -192,20 +77,30 @@ export default class PostScreen extends React.Component {
                         value={this.state.text}
                     />
                 </View>
-                <TouchableOpacity style={styles.photo} onPress={this.pickImage}>
+                {/* <TouchableOpacity style={styles.photo} onPress={this.pickImage}>
                     <Ionicons name='md-camera' size={32} color='#D8D9DB' />
-                </TouchableOpacity>
-                <View style={{ marginHorizontal: 32, marginTop: 32, height: 150 }}>
+                </TouchableOpacity> */}
+                {/* <View style={{ marginHorizontal: 32, marginTop: 32, height: 150 }}>
                     <Image
                         source={{ uri: this.state.image }}
                         style={{ width: '100%', height: '100%' }}
                     />
-                </View>
+                </View> */}
                 <View style={styles.footer}>
                     <TouchableOpacity onPress={this.handlePost.bind(this)}>
                         <Text style={{ height: 100, fontWeight: "500", color: 'blue' }}>Post</Text>
                     </TouchableOpacity>
                 </View>
+                <View>
+                    <Button
+                        buttonStyle={{backgroundColor:'red'}}
+                        // onPress={this.handlePost.bind(this)}
+                        title="Post"
+                        // color="#3b9fff"
+                        // accessibilityLabel="Learn more about this purple button"
+                    />
+                </View>
+                
             </SafeAreaView>
         );
     }
@@ -245,5 +140,16 @@ const styles = StyleSheet.create({
     photo: {
         alignItems: 'flex-end',
         marginHorizontal: 32
+    },
+    Button:{
+        marginRight:40,
+        marginLeft:40,
+        marginTop:10,
+        paddingTop:10,
+        paddingBottom:10,
+        backgroundColor:'#3b9fff',
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#fff'
     }
 });
